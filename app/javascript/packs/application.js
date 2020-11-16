67c8fa2eb99090a477672c711599367d98cc6262
karolina langner <karolinalangner1@gmail.com>
2020-11-16 13:53:04 +0000

    app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,5 @@ import 'bootstrap';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
